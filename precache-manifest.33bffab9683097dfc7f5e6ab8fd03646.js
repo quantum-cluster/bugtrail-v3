ab2017fc64a89a1454bc26090d9546b78ef50a1d
Nikhil Choudhary <nc1336282@gmail.com>
2020-09-30 23:08:46 +0530

    Updates

---
+++ precache-manifest.33bffab9683097dfc7f5e6ab8fd03646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d789384bccded9408d974a5ab6dca4",
+    "url": "/bugtrail-v3/index.html"
+  },
+  {
+    "revision": "c609de98b89c7c25889e",
+    "url": "/bugtrail-v3/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ba60be8f0528909c1bd0",
+    "url": "/bugtrail-v3/static/css/main.f58bcd78.chunk.css"
+  },
+  {
+    "revision": "c609de98b89c7c25889e",
+    "url": "/bugtrail-v3/static/js/2.e5aa2503.chunk.js"
+  },
+  {
+    "revision": "b0766469f31048aca440c2741ce67672",
+    "url": "/bugtrail-v3/static/js/2.e5aa2503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba60be8f0528909c1bd0",
+    "url": "/bugtrail-v3/static/js/main.c0f72247.chunk.js"
+  },
+  {
+    "revision": "114353419efe9e3548bf",
+    "url": "/bugtrail-v3/static/js/runtime-main.6f2129a0.js"
+  }
+]);
